0cc87a34cc22310b925558405af2729960754bf5
Rocket <84031530+Rocketlauncher269@users.noreply.github.com>
2025-06-02 22:06:01 +0100

    Snowy chain-link fence + Localisation

---
+++ Content/Tiles/Walls/ChainlinkFenceWithSnow.cs
@@ -0,0 +1,36 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace TheAuroraMod.Content.Tiles.Walls
+{
+	public class ChainlinkFenceWithSnow: ModItem
+    {
+	public override void SetStaticDefaults()
+	{
+	//DisplayName.SetDefault("Chain-link Fence");
+	
+	//Tooltip.SetDefault("''");
+
+        Item.ResearchUnlockCount = 400;
+
+	}
+	public override void SetDefaults()
+	{
+	    Item.width = 24;
+	    Item.height = 24;
+	    Item.value = Item.sellPrice(gold: 0);
+	    Item.rare = ItemRarityID.White;
+	    Item.DefaultToPlaceableWall(ModContent.WallType<ChainlinkFenceWithSnowPlaced>());
+	}
+
+	public override void AddRecipes()
+	{
+		CreateRecipe(4)
+		.AddIngredient(ItemID.Chain, 5)
+		.AddRecipeGroup(RecipeGroupID.IronBar, 3)
+		.AddTile(TileID.Anvils)
+		.Register();
+	}
+    }
+}
