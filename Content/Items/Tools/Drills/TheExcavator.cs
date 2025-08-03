d039c54575e489bee2c48b395fa3ded57b2c6f60
clyder8 <109995796+clyder8@users.noreply.github.com>
2025-08-03 10:21:15 -0400

    Drill

---
+++ Content/Items/Tools/Drills/TheExcavator.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using TheAuroraMod.Content.Projectiles.Tools;
+
+namespace TheAuroraMod.Content.Items.Tools.Drills
+{
+    public class TheExcavator : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+
+        }
+
+        public override void SetDefaults()
+        {
+            Item.damage = 2;
+            Item.DamageType = DamageClass.Melee;
+            Item.width = 44;
+            Item.height = 28;
+            Item.useTime = 2;
+            Item.useAnimation = 15;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.channel = true;
+            Item.knockBack = 0.5f;
+            Item.UseSound = SoundID.Item23;
+            Item.autoReuse = true;
+            Item.useTurn = true;
+            Item.pick = 35;
+            Item.noUseGraphic = true;
+            Item.noMelee = true;
+            Item.shoot = ModContent.ProjectileType<TheExcavatorProjectile>();
+            Item.shootSpeed = 32;
+        }
+    }
+}
